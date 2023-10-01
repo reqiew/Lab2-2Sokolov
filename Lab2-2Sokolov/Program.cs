282da0d2a0cfe7c4e0d0caf288f3c75fd6cd72fd
dobry <dobry@DESKTOP-FBD3PAQ>
2023-10-01 12:37:03 +0300

    Добавьте файлы проекта.

---
+++ Lab2-2Sokolov/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите x");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+double y = double.Parse(Console.ReadLine());
+if (y * y + (x - 1) * (x - 1) == 1) Console.WriteLine("Входит");
+else Console.WriteLine("Не входит");
